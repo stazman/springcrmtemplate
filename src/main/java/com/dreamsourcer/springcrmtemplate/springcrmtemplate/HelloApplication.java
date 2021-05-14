582fe733a9749940cc7f4bb695b40b4b44cd9b13
Christopher DiStasio <christopherdistasio@gmail.com>
2021-05-14 18:55:17 -0500

    Creates initial project files

---
+++ src/main/java/com/dreamsourcer/springcrmtemplate/springcrmtemplate/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.dreamsourcer.springcrmtemplate.springcrmtemplate;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
